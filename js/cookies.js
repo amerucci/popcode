834473ad575913b07d6f921653e39d1ee9086375
amerucci <a.merucci@onlineformapro.com>
2022-07-20 14:55:35 +0200

    finalizing cookie and analytics

--- js/cookies.js
+++ js/cookies.js
@@ -46,7 +46,6 @@ function acceptCookieConsent(){
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-    
     gtag('config', 'G-ZWKNYV187G');
     document.getElementById("cookieNotice").style.display = "none";
 }
